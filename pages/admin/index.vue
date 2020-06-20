109b8f426e8c2b652816e34769b6a8442ec4ae60
Tech Reagan <reaganekhameye@gmail.com>
2020-06-20 20:50:16 +0100

    Add validation to admin login page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,21 +5,47 @@
         <v-card-title class="headline pl-8">
           Login
         </v-card-title>
-        <v-card-text>
-          <v-text-field label="Email" solo></v-text-field>
-
-          <v-text-field
-            label="Password"
-            solo
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPassword ? 'text' : 'password'"
-            @click:append="showPassword = !showPassword"
-          ></v-text-field>
-        </v-card-text>
-        <v-card-actions class="pl-5 pb-5 flex justify-space-between">
-          <v-btn color="primary">Login</v-btn>
-          <v-btn text small>Forgot Password?</v-btn>
-        </v-card-actions>
+        <ValidationObserver v-slot="{ handleSubmit, reset }">
+          <form
+            ref="form"
+            @submit.prevent="handleSubmit(login)"
+            @reset.prevent="reset"
+          >
+            <v-card-text>
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="Email"
+                rules="required|email"
+              >
+                <v-text-field
+                  v-model="email"
+                  :error-messages="errors"
+                  label="Email"
+                  solo
+                ></v-text-field>
+              </ValidationProvider>
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="Password"
+                rules="required|min:5"
+              >
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  solo
+                  :error-messages="errors"
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="showPassword ? 'text' : 'password'"
+                  @click:append="showPassword = !showPassword"
+                ></v-text-field>
+              </ValidationProvider>
+            </v-card-text>
+            <v-card-actions class="pl-5 pb-5 flex justify-space-between">
+              <v-btn type="submit" color="primary">Login</v-btn>
+              <v-btn text small>Forgot Password?</v-btn>
+            </v-card-actions>
+          </form>
+        </ValidationObserver>
       </v-card>
     </v-col>
   </v-row>
@@ -29,7 +55,20 @@
 export default {
   data() {
     return {
-      showPassword: false
+      showPassword: false,
+      password: '',
+      email: ''
+    }
+  },
+  methods: {
+    login() {
+      // eslint-disable-next-line
+      console.log('hello')
+
+      // this.$refs.form.setErrors({
+      //       Email: ["We don't reconize, this email"],
+      //       Password: ["We don't reconize, this password"]
+      //     })
     }
   }
 }
